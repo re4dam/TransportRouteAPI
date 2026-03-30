65fa18fcd1f15b8082b1cf5d78f7cd10508f446c
re4dam <zakiadam332@gmail.com>
2026-03-30 16:34:54 +0700

    Added UserDto for Admin to access

---
+++ DTOs/Users/UserResponseDto.cs
@@ -0,0 +1,8 @@
+namespace TransportRouteApi.DTOs;
+
+public class UserResponseDto
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Role { get; set; } = "User"; // Default role is "User"
+}
