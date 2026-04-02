a4a9d4df2ba7ffb1e030f2a165ab3a34d068a179
re4dam <zakiadam332@gmail.com>
2026-04-02 09:25:07 +0700

    Add DTOs for Forgot Password and Reset Password functionality

---
+++ DTOs/Users/ForgotPasswordDto.cs
@@ -0,0 +1,6 @@
+namespace TransportRouteApi.DTOs;
+
+public class ForgotPasswordDto
+{
+    public string Username { get; set; } = string.Empty;
+}
---
+++ DTOs/Users/ResetPasswordDto.cs
@@ -0,0 +1,7 @@
+namespace TransportRouteApi.DTOs;
+
+public class ResetPasswordDto
+{
+    public string Token { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
